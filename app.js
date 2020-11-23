106612f27339fbc605d10aded95663b9ca74fef4
David <ds90289@yahoo.com>
2020-11-23 23:02:43 +0800

    all pages rendered are ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 var faker = require('faker');
 
@@ -13,23 +14,23 @@ app.get("/", function(req, res) {
 	var randomName = faker.name.findName(); // Rowan Nikolaus
     var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
     // var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-	res.render("index.html", { name: randomName });
+	res.render("index", { name: randomName });
 });
 
 app.get('/lesson1', function(req, res) {
-	res.render("lesson1.html");
+	res.render("lesson1.ejs");
 })
 
 app.get('/lesson2', function(req, res) {
-	res.render("lesson2.html");
+	res.render("lesson2");
 })
 
 app.get('/lesson3', function(req, res) {
-	res.render("lesson3.html");
+	res.render("lesson3");
 })
 
 app.get('/lesson4', function(req, res) {
-	res.render("lesson4.html");
+	res.render("lesson4");
 })
 
 app.listen(port, function() {
